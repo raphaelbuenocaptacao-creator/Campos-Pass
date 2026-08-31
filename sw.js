2f90621959e9828c7669c5b41223407509408c6d
raphaelbuenocaptacao-creator <raphaelbueno.captacao@gmail.com>
2026-08-31 04:13:56 -0300

    fix(pwa): harden Campos Pass sensitive-query cache guard

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'campos-pass-shell-v3-safe';
+const CACHE_NAME = 'campos-pass-shell-v4-safe';
 const OFFLINE_URL = './';
 const STATIC_ASSETS = [
   './',
@@ -10,7 +10,7 @@ const STATIC_ASSETS = [
 ];
 
 const PRIVATE_PATH_RE = /\/(api|auth|login|logout|admin|session|sessions|token|tokens|account|profile|me)(\/|$)/i;
-const SENSITIVE_QUERY_RE = /^(token|access_token|refresh_token|password|secret|session|auth|authorization|api[_-]?key|key|code|credential|credentials)$/i;
+const SENSITIVE_QUERY_RE = /^(token|access_token|refresh_token|password|passwd|secret|session|auth|authorization|api[_-]?key|key|code|credential|credentials)$/i;
 const STATIC_PATHS = new Set(STATIC_ASSETS.map(asset => new URL(asset, self.registration.scope).pathname));
 
 function hasSensitiveQuery(url) {
